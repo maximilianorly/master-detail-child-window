3415d4ce562cad0a093aede6d679bc6a1268e252
Maximilian <maximilian.riley@outlook.com>
2023-12-07 13:26:58 +0000

    Proof of concept complete

---
+++ public/child-window/script.js
@@ -0,0 +1,31 @@
+window.addEventListener("message", (event) => {
+    if (
+        // accept only from specified origin
+        event.origin !== "http://localhost:3000"
+        // handle interval messages from other sources than the opening window, i.e. react-dev-tools -.-
+        || event.source !== window.opener) {
+        return;
+    }
+    if (event.data) {
+        try {
+            var data = JSON.parse(event.data);
+            var displayArea = document.getElementById('dataDisplay');
+        
+            displayArea.textContent = `Name: ${data.name}, Age: ${data.age}, Shoe Size: ${data.shoeSize}`;
+        } catch (err) {
+            console.error('Incorrect data format sent to window. Message:', event.data);
+            return;
+        }
+    }
+});
+
+document.getElementById('closeAndOpenModal').addEventListener('click', () => {
+    window.opener.reopenModalOnChildWindowClose();
+    window.close();
+});
+
+window.onunload = () => {
+    if (window.opener && !window.opener.closed) {
+        window.opener.handleButtonsPureReset();
+    }
+};
